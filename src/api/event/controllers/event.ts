be9387a45a440a63ab9fcb53ca5e8d2b6631955a
Henry Okonkwo <henry@artnight.com>
2023-08-28 20:51:44 +0200

    Extend event controller logic to validate events with same start date-time and host

--- src/api/event/controllers/event.ts
+++ src/api/event/controllers/event.ts
@@ -4,4 +4,21 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::event.event');
+export default factories.createCoreController('api::event.event', ({ strapi }) => ({
+  async create(ctx) {
+    const { startDate, host } = ctx.request.body;
+
+    // const eventService = (strapi.services as any).event;
+    
+    // Check if an event exists with the same startDate and host
+    const existingEvent = await super.findOne({ startDate, host });
+
+    if (existingEvent) {
+      return ctx.throw(400, 'An event with the same startDate already exists for this host.');
+    }
+
+    // Calling the default core action
+    return existingEvent.data;
+  },
+}));
+
