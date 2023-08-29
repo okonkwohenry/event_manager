38c717178d7686d741ef4efdadf4d50d4d879513
Henry Okonkwo <henry@artnight.com>
2023-08-30 01:31:15 +0200

    Configure database for heroku

--- config/database.ts
+++ config/database.ts
@@ -1,45 +1,56 @@
-import path from 'path';
+// import path from 'path';
 
-export default ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'sqlite');
+// export default ({ env }) => {
+//   const client = env('DATABASE_CLIENT', 'sqlite');
 
-  const connections = {
-    postgres: {
-      connection: {
-        connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env('DATABASE_SSL', false),
-        schema: env('DATABASE_SCHEMA', 'public'),
-      },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
-    },
-  };
+//   const connections = {
+//     postgres: {
+//       connection: {
+//         connectionString: env('DATABASE_URL'),
+//         host: env('DATABASE_HOST', 'localhost'),
+//         port: env.int('DATABASE_PORT', 5432),
+//         database: env('DATABASE_NAME', 'strapi'),
+//         user: env('DATABASE_USERNAME', 'strapi'),
+//         password: env('DATABASE_PASSWORD', 'strapi'),
+//         ssl: env('DATABASE_SSL', false),
+//         schema: env('DATABASE_SCHEMA', 'public'),
+//       },
+//       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+//     },
+//   };
+
+//   return {
+//     connection: {
+//       client,
+//       ...connections[client],
+//       acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+//     },
+//   };
+// };
+
+import { parse } from 'pg-connection-string';
+
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
 
   return {
-    connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+    defaultConnection: "default",
+    connections: {
+      default: {
+        connector: "bookshelf",
+        settings: {
+          client: "postgres",
+          host,
+          port,
+          database,
+          username: user,
+          password,
+          ssl: { rejectUnauthorized: false }
+        },
+        options: {
+          ssl: false
+        },
+      },
     },
   };
 };
-
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'postgres',
-//     connection: {
-//       host: env('DATABASE_HOST', '127.0.0.1'),
-//       port: env.int('DATABASE_PORT', 5432),
-//       database: env('DATABASE_NAME', 'strapi'),
-//       user: env('DATABASE_USERNAME', 'strapi'),
-//       password: env('DATABASE_PASSWORD', 'strapi'),
-//       schema: env('DATABASE_SCHEMA', 'public'), // Not required
-//       ssl: env('DATABASE_SSL', false),
-//     },
-//     debug: false,
-//   },
-// });
